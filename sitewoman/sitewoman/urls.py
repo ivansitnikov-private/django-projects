580d42c3151e7f437bd2df15c446616e1770e03c
sitnikov ivan <sitnikov_ivan@outlook.com>
2026-03-29 16:16:59 +0300

    Registrsation views in urls module

--- sitewoman/sitewoman/urls.py
+++ sitewoman/sitewoman/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from women import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('women/', views.index),
+    path('cats/', views.categories),
 ]
+
