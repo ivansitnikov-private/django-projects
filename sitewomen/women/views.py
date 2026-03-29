50590c377e2839ba3714c283d366e8039510f166
sitnikov ivan <sitnikov_ivan@outlook.com>
2026-03-30 01:46:54 +0300

    Create dynamic pages

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,5 +7,12 @@ def index(request: HttpRequest) -> HttpResponse:
     """ Функция-представление для теста отображения главной страницы"""
     return HttpResponse("Страница приложения women")
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p> id {cat_id}</p>")
+ 
+def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p> slug {cat_slug} </p>")
+
+def archive(request: HttpRequest, year: str) -> HttpResponse:
+    return HttpResponse(f"<h1>Архив</h1><p> year {year} </p>")
+ 
